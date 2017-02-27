8631cc33df5889f1dbb5911c3e8fd4c79b2a2da9
lihao_j1 <lihao@j1.com>
2017-02-27 13:57:51 +0800

    导入空项目

---
+++ AvoidCommonCrashDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  AvoidCommonCrashDemo
+//
+//  Created by j1 on 17/2/27.
+//  Copyright © 2017年 j1. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ AvoidCommonCrashDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  AvoidCommonCrashDemo
+//
+//  Created by j1 on 17/2/27.
+//  Copyright © 2017年 j1. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
